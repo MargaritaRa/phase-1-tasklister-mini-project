fb68cb4762c09ec45bf0fb74eaaf8155902b88e6
Margarita Ramirez <margarita26.mr@gmail.com>
2024-03-14 08:28:36 -0400

    add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.querySelector("#create-task-form").addEventListener("submit", event => {
+  event.preventDefault();
+  console.log("The form was submitted");
+  const submittedDescription = document.querySelector("#new-task-description").value;
+  console.log(submittedDescription);
+ 
 });
+
+const input = document.querySelector("input[type='search']");
+input.addEventListener("search", event => {
+  console.log(`The term searched for was ${input.value}`);
+
+});
+
+const input2 = document.querySelector('input[type="search"]');
+
+input2.onsearch = () => {
+  console.log(`The term searched for was ${input2.value}`);
+
+};
